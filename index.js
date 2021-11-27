e18254f946cdc5efc80e0011e66d4a4862be6f35
vivekraj_kr <vivekrajkr.mail@gmail.com>
2021-11-27 22:44:17 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react';
+import keypress from 'keypress.js';
+
+const KeyboardShortcut = ({ combo, callback, children }) => {
+  useEffect(() => {
+    const listener = new keypress.Listener();
+    listener.simple_combo(combo, callback);
+  }, [combo, callback])
+
+  return <div> {children} </div>
+}
+
+export default KeyboardShortcut;
